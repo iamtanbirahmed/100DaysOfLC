8af64a0b3955ce5cb336bee324b06d0f2a1157f7
iamtanbirahmed <tanbir.sagar@gmail.com>
2021-12-31 13:33:24 -0500

    [206] Reverse Linked List

--- 206.java
+++ 206.java
@@ -10,17 +10,14 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null) return head;
-        ListNode prev = head;
-        ListNode curr = prev.next;
-        prev.next = null;
-        while(curr != null){
-            ListNode nxt = curr.next;
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr !=null){
+            ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
-            curr = nxt;
+            curr = next;
         }
         return prev;
-        
     }
 }
