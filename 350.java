e9bf169243cad016e8a07f5e0792fa8ff6d2d00a
iamtanbirahmed <tanbir.sagar@gmail.com>
2022-02-28 15:56:17 -0500

    [350] Intersection of Two Arrays II

---
+++ 350.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        List<Integer> result = new ArrayList();
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i] < nums2[j]){
+                i++;
+            }else if(nums1[i]>nums2[j]){
+                j++;
+            }else{
+                result.add(nums1[i]);
+                i++;
+                j++;
+            }            
+        }
+        
+        int[] res = new int[result.size()];
+        for(i=0;i<res.length;i++){
+            res[i] = result.get(i);
+        }
+        
+        return res;
+    }
+}
