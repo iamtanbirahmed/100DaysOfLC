4acbf90c2a67914aeff3b9351f851034c4aebcec
iamtanbirahmed <tanbir.sagar@gmail.com>
2022-05-21 23:23:10 +0600

    [47] Permutaions II (updated)

--- 47.java
+++ 47.java
@@ -1,33 +1,31 @@
 class Solution {
-    private ArrayList<List<Integer>> result = new ArrayList();
-    private void findPermutations(LinkedList<Integer> currentList, int n, int[] nums, HashMap<Integer, Integer> count){
-        if(count.get(n) == 0) return;
-        currentList.add(n);
-        count.put(n,count.get(n)-1);
-        if(currentList.size() == nums.length){
+    
+    private List<List<Integer>> result =new LinkedList();;
+    
+    private void findPermutations(LinkedList<Integer> currentList, Map<Integer, Integer> count, int length){
+        if(currentList.size() > length) return;
+        if(currentList.size() == length){
             this.result.add(new LinkedList(currentList));
         }
         
-        for(int num: count.keySet()){
-            this.findPermutations(currentList, num, nums, count);
-        }
-        count.put(n, count.get(n)+1);
-        currentList.removeLast();
-    }
-    public List<List<Integer>> permuteUnique(int[] nums) {
-        HashMap<Integer, Integer> count = new HashMap();
-        for(int n: nums){
-            if(count.containsKey(n) == false){
-                count.put(n, 1);
-                continue;
-            }
-            count.put(n, count.get(n)+1);
-        }
         for(int n: count.keySet()){
-            this.findPermutations(new LinkedList<Integer>(),n, nums, count);    
+            if(count.get(n) <= 0 )
+                continue;
+            count.put(n, count.get(n)-1);
+            currentList.add(n);
+            this.findPermutations(currentList, count, length);
+            count.put(n, count.get(n)+1);
+            currentList.removeLast();
+        }
+    }
+    
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Map<Integer, Integer> count = new HashMap();
+        for(int n: nums){
+            count.put(n, count.getOrDefault(n,0)+1);
         }
         
+        this.findPermutations(new LinkedList<Integer>(), count, nums.length); 
         return this.result;
-        
     }
 }
