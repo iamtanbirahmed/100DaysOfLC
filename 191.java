db86533b477af042d6cf67293bd63c92b3b3cb44
Tanbir Ahmed <35971692+iamtanbirahmed@users.noreply.github.com>
2022-05-26 17:17:27 +0000

    [191] Number of 1 Bit

--- 191.java
+++ 191.java
@@ -2,12 +2,14 @@ public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         int count = 0;
-        
         for(int i=0;i<32;i++){
-            int lastBit = (n>>i)&1;
-            if(lastBit == 1)count++;
+            int sign = (n>>i) & 1;
+            if(sign==1){
+                count++;    
+            }
+            
         }
-        return count;
         
+        return count;
     }
 }
