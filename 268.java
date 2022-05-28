e042fddc21aee37ce7984aaee3d48bb74667bcbc
Tanbir Ahmed <35971692+iamtanbirahmed@users.noreply.github.com>
2022-05-28 11:59:52 +0000

    [268] Missing Number

--- 268.java
+++ 268.java
@@ -1,11 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int exp = ((n+1-1)*(n+2-1))/2;
-        
-        for(int i=0;i<n;i++){
-            exp= exp - nums[i];
+        int missing = 0;
+        for(int i=0;i<=nums.length;i++){
+            missing^=i;
         }
-        return exp;
+        for(int n: nums){
+            missing^=n;
+        }        
+        return missing;
     }
 }
