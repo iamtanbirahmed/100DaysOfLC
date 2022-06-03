30b783ca9cce484cb242f63834b5142e7fd7ccae
Tanbir Ahmed <35971692+iamtanbirahmed@users.noreply.github.com>
2022-06-03 16:52:40 +0000

    [669] Trim a Binary Search Tree

---
+++ 669.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if(root == null) return null;
+        if(root.val < low) return this.trimBST(root.right, low, high);
+        else if(root.val > high) return this.trimBST(root.left, low, high);
+        root.left = this.trimBST(root.left, low, high);
+        root.right = this.trimBST(root.right, low, high);
+        
+        return root;
+    }
+}
