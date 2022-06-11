61b18c50cc00209503aed57e11eb6dd279adfb0e
Tanbir Ahmed <35971692+iamtanbirahmed@users.noreply.github.com>
2022-06-11 17:36:31 +0000

    [1332] Remove Palindromic Subsequences

--- 1332.java
+++ 1332.java
@@ -1,19 +1,20 @@
 class Solution {
+    
     private boolean isPalindrom(String s){
         int left = 0;
         int right = s.length()-1;
-        while(left<=right){
+        while(left<right){
             if(s.charAt(left)!=s.charAt(right))
                 return false;
             left++;
             right--;
         }
-        
         return true;
     }
+    
     public int removePalindromeSub(String s) {
         if(this.isPalindrom(s))
             return 1;
-        else return 2;
+        return 2;
     }
 }
