af0d879bc452246b895f391f33c08c7492a9e03a
Tanbir Ahmed <35971692+iamtanbirahmed@users.noreply.github.com>
2022-06-22 13:37:38 +0000

    [695] Max Area of Island

--- 695.java
+++ 695.java
@@ -3,38 +3,37 @@ class Solution {
     private boolean isValid(int x, int y, int[][] grid){
         int m = grid.length;
         int n = grid[0].length;
-        if(x<0 || x>=m || y<0 || y>=n || grid[x][y] == 0){
+        if(x<0|| x>=m || y<0 || y>=n|| grid[x][y]==0){
             return false;
         }
         return true;
     }
     
-    private int countArea(int x, int y, int[][] grid, int count){
-        if(this.isValid(x, y, grid) == false) return count;
-        grid[x][y] = 0;
-        count++;
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,-1,1};
+    private int dfs(int x, int y, int[][] grid, int currentArea){
+        if(this.isValid(x, y, grid) == false) return currentArea;
+        grid[x][y] = 0;        
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {-1, 1, 0, 0};
         for(int i=0;i<4;i++){
-            count= this.countArea(x+dx[i], y+dy[i], grid, count);
+            currentArea = this.dfs(x+dx[i], y+dy[i], grid, currentArea);
         }
-        return count;
+        return ++currentArea;
     }
     
-    
-    
     public int maxAreaOfIsland(int[][] grid) {
-        int maxArea = 0;
+        int max = Integer.MIN_VALUE;
         int m = grid.length;
         int n = grid[0].length;
-        for( int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                
-                if(grid[i][j] == 1){
-                    maxArea = Math.max(maxArea, this.countArea(i,j, grid, 0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){                
+                if(grid[i][j]==1){
+                    Integer currentArea = 0;
+                    currentArea = this.dfs(i, j, grid, currentArea);
+                    max = Math.max(max, currentArea);
                 }
             }
         }
-        return maxArea;
+        
+        return (max == Integer.MIN_VALUE) ? 0 : max;
     }
 }
