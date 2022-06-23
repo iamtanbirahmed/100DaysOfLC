5208156514f7f2c63e76d31a1a243ddccbf6e03f
Tanbir Ahmed <35971692+iamtanbirahmed@users.noreply.github.com>
2022-06-23 20:45:42 +0000

    [994] Rotting Oranges

--- 994.java
+++ 994.java
@@ -1,60 +1,54 @@
 class Solution {
-    
     private boolean isValid(int x, int y, int[][] grid){
         int m = grid.length;
         int n = grid[0].length;
-        if(x<0 || x>=m || y<0 || y>=n || grid[x][y] != 1){
+        if(x<0|| x>=m || y<0 || y>=n || grid[x][y]!=1){
             return false;
         }
         return true;
     }
-    
     public int orangesRotting(int[][] grid) {
-        Queue<Pair<Integer, Integer>> queue = new LinkedList();
+        int freshOrange = 0;
+        Queue<int[]> queue = new LinkedList();
         int m = grid.length;
         int n = grid[0].length;
-        
-        int fresh = 0;
-        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j] == 2){
-                    queue.add(new Pair(i,j));
-                }else if(grid[i][j] == 1){
-                    fresh++;
+                if(grid[i][j] == 1){
+                    freshOrange++;                    
+                }else if(grid[i][j] == 2){
+                    queue.offer(new int[]{i,j});
                 }
             }
         }
-        
-        queue.add(new Pair(-1,-1));
-        int count = 0;
+        int min = 0;
+        queue.offer(new int[] {-1,-1});
         while(queue.isEmpty() == false){
-            Pair<Integer, Integer> pair = queue.poll();
-            int x = pair.getKey();
-            int y = pair.getValue();
-            
-            if(x < 0 || y< 0){
-                if(queue.isEmpty() == false){
-                    queue.add(new Pair(-1,-1));
-                    count++;
-                }
+            int[] src = queue.poll(); 
+            int x = src[0];
+            int y = src[1];
+            if(x == -1 && y==-1){
+                if(queue.isEmpty() == true)
+                    break;
+                min++;
+                queue.offer(new int[]{-1,-1});
                 continue;
-                
-                
             }
-            
-            int[] dx = {1,-1,0,0};
+            int[] dx = {-1,1,0,0};
             int[] dy = {0,0,-1,1};
-            
             for(int i=0;i<4;i++){
-                if(this.isValid(x+dx[i], y+dy[i], grid) == true){
-                    fresh--;
-                    grid[x+dx[i]][y+dy[i]] = 2;
-                    queue.add(new Pair(x+dx[i], y+dy[i]));
+                int X = x+dx[i];
+                int Y = y+dy[i];
+                if(this.isValid(X,Y, grid) == false){
+                    continue;
                 }
+                grid[X][Y] = 2;
+                freshOrange--;
+                queue.offer(new int[]{X,Y});
             }
+            
         }
         
-        return (fresh == 0) ? count : -1;
+        return (freshOrange == 0) ? min : -1;
     }
 }
