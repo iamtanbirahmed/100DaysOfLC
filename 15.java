da43c61986cfdc1a9f141c6e26e36aaf6381890c
Tanbir Ahmed <35971692+iamtanbirahmed@users.noreply.github.com>
2022-06-28 17:30:36 +0000

    [15] 3Sum

--- 15.java
+++ 15.java
@@ -1,26 +1,27 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
         Set<List<Integer>> result = new HashSet();
-        for(int i=0;i<nums.length;i++){
-            int left = i+1;
-            int right = nums.length-1;
-            int target = nums[i]*(-1);
+        Arrays.sort(nums);
+        
+        for(int i=nums.length-1;i>=0;i--){
+            int target = (-1)*nums[i];
+            int left = 0;
+            int right = i-1;
+            
             while(left<right){
-                if(nums[left]+nums[right] > target){
+                int sum = nums[left]+nums[right];
+                if(sum == target){
+                    result.add(Arrays.asList(new Integer[]{nums[i], nums[left], nums[right]}));
                     right--;
-                }else if(nums[left]+nums[right]< target){
                     left++;
-                }else{
-                    ArrayList<Integer> sln = new ArrayList();
-                    sln.addAll(Arrays.asList(nums[i], nums[left], nums[right]));
-                    result.add(sln);
+                }else if(sum>target){
                     right--;
+                }else{
                     left++;
                 }
             }
-            
         }
+        
         return new ArrayList(result);
     }
 }
