b385d3e55395a60169366e2be18ea85a6628ca74
Tanbir Ahmed <35971692+iamtanbirahmed@users.noreply.github.com>
2022-07-07 08:58:35 +0000

    [79] Word Search.

--- 79.java
+++ 79.java
@@ -1,45 +1,45 @@
 class Solution {
     
-    private boolean isValid(int x, int y, char[][] board, boolean[][] visited){
-        if(x <0 || x>= board.length || y<0 || y>= board[0].length || visited[x][y] == true){
+    private boolean isValid(int x, int y, char[][] grid){
+        int m = grid.length;
+        int n = grid[0].length;
+        if(x<0 || x>=m || y<0 || y>=n || grid[x][y] == '#')
             return false;
-        }
         return true;
     }
     
-    
-    public boolean searchWord(int x, int y, char[][] board, int index, String word, boolean[][] visited) {
-        if(this.isValid(x, y, board, visited) == false || index >= word.length() || word.charAt(index)!= board[x][y]){
-            return false;
-        }
-        if(index == word.length()-1)
-            return true;
-        visited[x][y] = true;
-        index++;
-        int[] dx = {0,0,-1,1};
-        int[] dy = {-1,1,0,0};
-        boolean found = false;        
+    private boolean findWord(int index, int x, int y, char[][] grid, String word){
+        if(this.isValid(x, y, grid) == false || index>=word.length()) return false;
+        if(word.charAt(index)!= grid[x][y]) return false;
+        if(index == word.length()-1) return true;
+        
+        boolean found = false;
+        char prev = grid[x][y];
+        grid[x][y] = '#';
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
         for(int i=0;i<4;i++){
-            found  = this.searchWord(x+dx[i], y+dy[i], board, index, word, visited);
-            if(found == true){
+            found = this.findWord(index+1, x+dx[i], y+dy[i], grid, word);
+            if(found == true)
                 break;
-            }
         }
-        index--;
-        visited[x][y] =false;
+        grid[x][y] = prev;
         return found;
+        
     }
     
     public boolean exist(char[][] board, String word) {
         boolean found = false;
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j< board[0].length;j++){
-                found = this.searchWord(i,j, board, 0, word, new boolean[board.length][board[0].length]);
-                if (found == true){
+        int m = board.length;
+        int n = board[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                found = this.findWord(0, i, j, board, word);
+                if(found == true)
                     return true;
-                } 
             }
         }
-        return found;
+        
+        return false;
     }
 }
