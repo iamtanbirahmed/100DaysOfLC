5ef5d23f0d1087034dfd59b0a2fb4599bb1bd8be
Tanbir Ahmed <35971692+iamtanbirahmed@users.noreply.github.com>
2022-07-20 12:04:34 +0000

    [33] Search in Rotated Sorted Array.

--- 33.java
+++ 33.java
@@ -1,45 +1,40 @@
 class Solution {
-    
-    private int findRotatePivot(int[] nums){
+    private int findPivotIndex(int[] nums){
         int left = 0;
         int right = nums.length-1;
-        if(nums[left]<nums[right]) return -1;
-        while(left < right){
-            int mid = (left+right)/2;
-            if(nums[mid] > nums[mid+1])
+        while(left<=right){
+            int mid = left +(right-left)/2;            
+            if(nums[mid] > nums[mid+1]){
+                return mid+1;
+            }else if(nums[mid] < nums[mid-1]){
                 return mid;
-            if(nums[mid-1] > nums[mid])
-                return mid-1;
-            if(nums[mid] > nums[0]){
+            }else if(nums[mid] > nums[0]){
                 left = mid+1;
             }else if(nums[mid] < nums[0]){
-                right = mid-1;
-            }
+                right = mid - 1;
+            }             
         }
-        
-        return -1;
+        return -1;        
     }
     private int binarySearch(int[] nums, int left, int right, int target){
         while(left<=right){
             int mid = (left+right)/2;
-            if(nums[mid]>target){
-                right = mid-1;
-            }else if(nums[mid]<target){
-                left = mid+1;
-            }else{
+            if(nums[mid] == target){
                 return mid;
+            }else if(nums[mid] > target){
+                right = mid-1;
+            }else{
+                left = mid+1;
             }
         }
         return -1;
     }
-    
     public int search(int[] nums, int target) {
-        int pivotIndex = this.findRotatePivot(nums);
-        if(pivotIndex <0){
+        if(nums[0] <= nums[nums.length-1])
             return this.binarySearch(nums, 0, nums.length-1, target);
-        }
-        int left = this.binarySearch(nums, 0, pivotIndex, target);
-        int right = this.binarySearch(nums, pivotIndex+1, nums.length-1, target);
-        return left>=0? left:right;
+        
+        int pivotIndex = this.findPivotIndex(nums);
+        int leftHalfIndex = this.binarySearch(nums, 0, pivotIndex-1, target);
+        return leftHalfIndex >=0 ? leftHalfIndex : this.binarySearch(nums, pivotIndex, nums.length-1, target);
     }
 }
