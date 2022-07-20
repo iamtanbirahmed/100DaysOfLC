f62c076b18ba2bf0084d8a0a2e8fbf6081707df9
Tanbir Ahmed <35971692+iamtanbirahmed@users.noreply.github.com>
2022-07-20 12:05:49 +0000

    [74] Search a 2D Matrix.

--- 74.java
+++ 74.java
@@ -2,19 +2,19 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
+        int total = m*n;
         int left = 0;
-        int right = (m*n)-1;
+        int right = total-1;
         while(left<=right){
             int mid = left+(right-left)/2;
             int row = mid/n;
             int col = mid%n;
-            int elem = matrix[row][col];
-            if(elem ==target){
+            if(matrix[row][col] == target)
                 return true;
-            }else if(elem<target){
-                left = mid+1;
+            else if(matrix[row][col] > target){
+                right = mid -1;
             }else{
-                right = mid-1;
+                left = mid+1;
             }
         }
         return false;
