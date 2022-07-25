4c6c4f7c76c370b58759040527f1ea613674b0ee
Tanbir Ahmed <35971692+iamtanbirahmed@users.noreply.github.com>
2022-07-25 15:50:59 +0000

    [90] Subsets II.

--- 90.java
+++ 90.java
@@ -1,28 +1,28 @@
 class Solution {
     
-    List<List<Integer>> result = new LinkedList();
+    private List<List<Integer>> result = new LinkedList();
     
-    private void findSubsets(LinkedList<Integer> currentList, Map<Integer, Integer> count, Integer[] nums, int index){
-        
+    private void findSubSets(LinkedList<Integer> currentList, Map<Integer, Integer> count, int index, Integer[] nums){
         this.result.add(new LinkedList(currentList));
         for(int i=index;i<nums.length;i++){
-            int key = nums[i];
-            if(count.get(key) <= 0) continue;
-            count.put(key, count.get(key)-1);
-            currentList.add(key);
-            this.findSubsets(currentList, count, nums, i);
-            count.put(key, count.get(key)+1);
+            if(count.get(nums[i])<=0)
+                continue;
+            count.put(nums[i], count.get(nums[i])-1);
+            currentList.add(nums[i]);
+            this.findSubSets(currentList, count, i, nums);
+            count.put(nums[i], count.get(nums[i])+1);
             currentList.removeLast();
+            
         }
     }
     
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Map<Integer, Integer> count = new HashMap();
         for(int n: nums){
-            count.put(n, count.getOrDefault(n,0)+1);
-        }        
-        Integer[] keys = count.keySet().toArray(new Integer[count.keySet().size()]);
-        this.findSubsets(new LinkedList<Integer>(), count, keys, 0);        
+            count.put(n, count.getOrDefault(n, 0)+1);
+        }
+        Integer[] keys = count.keySet().toArray(new Integer[count.size()]);
+        this.findSubSets(new LinkedList<Integer>(), count, 0, keys);
         return this.result;
     }
 }
