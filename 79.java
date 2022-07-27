7849a16cb46d05c29b31a56f27eb29c045cdec60
Tanbir Ahmed <35971692+iamtanbirahmed@users.noreply.github.com>
2022-07-27 21:39:06 +0000

    [79] Word Search

--- 79.java
+++ 79.java
@@ -1,40 +1,42 @@
 class Solution {
     
-    private boolean isValid(int x, int y, char[][] grid){
-        int m = grid.length;
-        int n = grid[0].length;
-        if(x<0 || x>=m || y<0 || y>=n || grid[x][y] == '#')
+    private boolean isValid(char[][] board, int x, int y){
+        int m = board.length;
+        int n = board[0].length;
+        if(x<0 || x>=m || y<0 || y>=n || board[x][y] == '#')
             return false;
+        
         return true;
     }
     
-    private boolean findWord(int index, int x, int y, char[][] grid, String word){
-        if(this.isValid(x, y, grid) == false || index>=word.length()) return false;
-        if(word.charAt(index)!= grid[x][y]) return false;
-        if(index == word.length()-1) return true;
-        
-        boolean found = false;
-        char prev = grid[x][y];
-        grid[x][y] = '#';
+    private boolean findWord(char[][] board, int x, int y, int index, String word){
+        if(this.isValid(board, x, y) == false || index>= word.length()|| word.charAt(index)!= board[x][y]) return false;        
+        if(index == word.length()-1){
+            return true;
+        }
+        char tmp = board[x][y];
+        board[x][y] = '#';
         int[] dx = {-1,1,0,0};
         int[] dy = {0,0,-1,1};
+        boolean found = false;
+        
         for(int i=0;i<4;i++){
-            found = this.findWord(index+1, x+dx[i], y+dy[i], grid, word);
+            found = this.findWord(board, x+dx[i], y+dy[i], index+1, word);
             if(found == true)
                 break;
         }
-        grid[x][y] = prev;
+        board[x][y] = tmp;
         return found;
-        
     }
     
     public boolean exist(char[][] board, String word) {
-        boolean found = false;
         int m = board.length;
         int n = board[0].length;
+        
+        boolean found = false;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                found = this.findWord(0, i, j, board, word);
+                found = this.findWord(board, i, j, 0, word);
                 if(found == true)
                     return true;
             }
