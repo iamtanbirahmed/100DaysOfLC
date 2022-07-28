49e63660f40c61d31cbe1b6178b669bc83e0cb3f
Tanbir Ahmed <35971692+iamtanbirahmed@users.noreply.github.com>
2022-07-28 18:00:34 +0000

    [55] Jump Game.

--- 55.java
+++ 55.java
@@ -2,11 +2,9 @@ class Solution {
     public boolean canJump(int[] nums) {
         int lastIndex = nums.length-1;
         for(int i=nums.length-2;i>=0;i--){
-            if(i+nums[i]>= lastIndex){
+            if(i+nums[i]>=lastIndex)
                 lastIndex = i;
-            }
         }
-        
         return lastIndex == 0;
     }
 }
