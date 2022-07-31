b1a9b22c0b735a1421a3c67b31c9220bdc4efaf7
Tanbir Ahmed <35971692+iamtanbirahmed@users.noreply.github.com>
2022-07-31 16:57:04 +0000

    [213] House Robber II.

--- 213.java
+++ 213.java
@@ -5,17 +5,17 @@ class Solution {
         dp[left] = nums[left];
         dp[left+1] = Math.max(dp[left], nums[left+1]);
         for(int i=left+2;i<=right;i++){
-            dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i]);
+            dp[i] = Math.max(dp[i-1], nums[i]+dp[i-2]);
         }
         return dp[right];
     }
     
     public int rob(int[] nums) {
         if(nums.length == 1) return nums[0];
-        if(nums.length == 2) return Math.max(nums[0], nums[1]);
-        int max1 = this.findMax(0, nums.length-2, nums);
-        int max2 = this.findMax(1, nums.length-1, nums);
+        else if(nums.length == 2) return Math.max(nums[0], nums[1]);
+        int max_1 = this.findMax(0, nums.length-2, nums);
+        int max_2 = this.findMax(1, nums.length-1, nums);
         
-        return Math.max(max1, max2);
+        return Math.max(max_1, max_2);
     }
 }
