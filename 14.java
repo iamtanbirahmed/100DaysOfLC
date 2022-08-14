d98eabf2f2c38b7ae69d493aefc62352bf5a168d
Tanbir Ahmed <35971692+iamtanbirahmed@users.noreply.github.com>
2022-08-14 21:43:20 +0000

    [14] Longest Common Prefix.

--- 14.java
+++ 14.java
@@ -1,6 +1,6 @@
 class TrieNode{
     
-    HashMap<Character, TrieNode> children;
+    Map<Character, TrieNode> children;
     boolean isWord;
     
     TrieNode(){
@@ -10,46 +10,31 @@ class TrieNode{
     
 }
 
+
 class Solution {
     
-    private TrieNode root;
-    
-    public Solution(){
-        this.root = new TrieNode();
-    }
-    
-    public void insert(String word){
-        TrieNode currentHead = this.root;
-        char[] chars = word.toCharArray();
-        for(int i=0;i<word.length();i++){
-            char ch = chars[i];
-            if(currentHead.children.containsKey(ch) == false){
-                currentHead.children.put(ch, new TrieNode());
+    public String longestCommonPrefix(String[] strs) {
+        
+        TrieNode root  = new TrieNode();
+        for(String str: strs){
+            TrieNode currentNode = root;
+            for(char ch: str.toCharArray()){
+                if(currentNode.children.containsKey(ch) == false){
+                    currentNode.children.put(ch, new TrieNode());
+                }
+                currentNode = currentNode.children.get(ch);
             }
-            currentHead = currentHead.children.get(ch);
+            currentNode.isWord = true;
         }
-        currentHead.isWord = true;
         
-    }
-    
-    public String searchPrefix(){
         StringBuilder sb = new StringBuilder();
-        TrieNode currentHead = this.root;
-        
-        while(currentHead.children.keySet().size() == 1 && currentHead.isWord == false){
-            for(Character key: currentHead.children.keySet()){
-                sb.append(key);
-                currentHead = currentHead.children.get(key);
-            }            
+        TrieNode currentNode = root;
+        while(currentNode.children.size() == 1 && currentNode.isWord == false){
+            for(char ch: currentNode.children.keySet()){
+                sb.append(ch);
+                currentNode  = currentNode.children.get(ch);
+            }
         }
         return sb.toString();
-        
-    }
-    
-    public String longestCommonPrefix(String[] strs) {
-        for(String word: strs){
-            this.insert(word);
-        }
-        return this.searchPrefix();
     }
 }
